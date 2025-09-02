742483cf7315b0b4b3e0026b73d6e34bc1023393
Dan1el71 <ddrmhk1@gmail.com>
2025-09-02 16:24:47 -0500

    feat/create new angularV20 project

--- src/app/login-component.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoginComponentDirective } from './login-component.directive';
-
-describe('LoginComponentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoginComponentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
