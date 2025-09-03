d4b5806e5d83cdb693b4fac09da3cb9a248d67a5
Dan1el71 <ddrmhk1@gmail.com>
2025-09-02 20:05:07 -0500

    feat: implement home component with example cards and update template

--- src/app/domains/home/home.ts
+++ src/app/domains/home/home.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { ExampleCard } from '../../shared/components/example-card/example-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ExampleCard],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  adminCards = [{
+    
+  }];
+  examples = [
+    {
+      title: 'Example 1',
+      imageUrl: 'https://placehold.co/450',
+      description: 'Description for example 1',
+    },
+    {
+      title: 'Example 2',
+      imageUrl: 'https://placehold.co/450',
+      description: 'Description for example 2',
+    },
+    {
+      title: 'Example 3',
+      imageUrl: 'https://placehold.co/450',
+      description: 'Description for example 3',
+    },
+  ];
+}
