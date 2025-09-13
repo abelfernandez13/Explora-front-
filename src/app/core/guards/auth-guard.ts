2e9d804a83d3dc1cad900c0a90f098f74ffe64da
Dan1el71 <ddrmhk1@gmail.com>
2025-09-13 06:04:35 -0500

    feat: implement authentication logic in authGuard to protect routes

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,5 +1,16 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStore } from '../auth/application/auth-store';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const auth = inject(AuthStore);
+
+  if (auth.isAuthenticated()) {
+    return true;
+  }
+
+  router.navigate(['/']);
+  return false;
 };
