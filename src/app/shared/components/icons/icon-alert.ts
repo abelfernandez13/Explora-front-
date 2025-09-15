5d7dd8d50cf444b18b1fddad2d0d7dbe58c20656
Dan1el71 <ddrmhk1@gmail.com>
2025-09-15 16:04:28 -0500

    feat: add icon components for alert, facebook, and google

---
+++ src/app/shared/components/icons/icon-alert.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-alert',
+  template: `
+    <svg class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden>
+      <path
+        fill-rule="evenodd"
+        d="M8.257 3.099c.366-.446.957-.577 1.463-.31 2.18 1.376 4.83 3.045 6.28 4.14 1.09.832.625 2.425-.74 2.425H4.5c-1.1 0-1.75-1.092-1.06-2.004 1.12-1.56 2.856-3.35 4.817-4.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  `,
+})
+export class IconAlert {}
---
+++ src/app/shared/components/icons/icon-facebook.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-facebook',
+  template: `
+    <svg class="w-4 h-4" viewBox="0 0 24 24" aria-hidden>
+      <path
+        fill="#1877F2"
+        d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.093 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.007 1.792-4.669 4.533-4.669 1.313 0 2.686.235 2.686.235v2.963h-1.514c-1.492 0-1.956.93-1.956 1.887v2.267h3.328l-.532 3.49h-2.796V24C19.612 23.093 24 18.1 24 12.073z"
+      />
+    </svg>
+  `,
+})
+export class IconFacebook {}
